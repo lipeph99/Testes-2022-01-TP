5d4a7b7030b2f4b93c3a9f15e976ba5b5f05689e
lipeph99 <lipeph99@gmail.com>
2022-05-28 13:28:35 -0300

    começo do projeto

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-def criaConta(newId):
-  return
-
 def main():
+
+  def criaConta(newId):
+    id.append(newId)
+    saldo.append(0)
+    return
+  
+  def printaTodas():
+    i=0
+    while (i<len(id)):
+      print(id[i],saldo[i])
+      i = i+1
+    return
   #Ler input e printar matriz
   id = []
   saldo = []
+  criaConta('1')
+  criaConta('2')
+  printaTodas()
 
 if __name__ == "__main__":
     main()
