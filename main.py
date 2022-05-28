941dbc2fedba142b0075e274878c94c93ade1a79
lipeph99 <lipeph99@gmail.com>
2022-05-28 14:40:48 -0300

    get cpf set cpf e varios testes

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def criaConta(_id):
-  id.append(_id)
-  saldo.append(0)
-  nome.append('')
-  cpf.append('')
-  return
+  pos = achaConta(_id)
+  if(pos==-1):
+    id.append(_id)
+    saldo.append(0)
+    nome.append('')
+    cpf.append('')
+    return "Conta criada"
+  else:
+    return "Já existe uma conta com esse id"
 
 def printaTodas():
   i=0
@@ -22,6 +26,17 @@ def achaConta(_id):
     i = i+1
   return pos
 
+def deletaConta(_id):
+  pos = achaConta(_id)
+  if(pos==-1):
+    return "Conta não encontrada"
+  else:
+    id.pop(pos)
+    saldo.pop(pos)
+    nome.pop(pos)
+    cpf.pop(pos)
+    return "conta " + _id + " deletada"
+
 def printaConta(_id):
   pos = achaConta(_id)
   if(pos==-1):
@@ -35,7 +50,6 @@ def getNome(_id):
     return "Conta não encontrada"
   else:
     return nome[pos]
-  return
 
 def setNome(_id,_nome):
   pos = achaConta(_id)
@@ -45,27 +59,91 @@ def setNome(_id,_nome):
     nome[pos] = _nome
     return "nome da conta " + id[pos] + " alterado para " + nome[pos]
 
+def getCPF(_id):
+  pos = achaConta(_id)
+  if(pos==-1):
+    return "Conta não encontrada"
+  else:
+    return cpf[pos]
+
+def setCPF(_id,_cpf):
+  pos = achaConta(_id)
+  if(pos==-1):
+    return "Conta não encontrada"
+  else:
+    cpf[pos] = _cpf
+    return "CPF da conta " + id[pos] + " alterado para " + cpf[pos]
   
-def test_cria():
+def test_criaRepetido():
+  criaConta('1')
+  assert criaConta('1') == "Já existe uma conta com esse id"
+  deletaConta('1')
+
+def test_criaOK():
   criaConta('1')
   assert printaConta('1') == "id = 1 saldo = 0 nome = "
+  deletaConta('1')
 
-def test_setNome():
-  criaConta('2')
-  assert setNome('2','joão') == "nome da conta 2 alterado para joão"
-
-def get_nome():
+def test_achaContaOK():
+  criaConta('1')
+  assert achaConta('1') == 0
+  deletaConta('1')
   return
+
+def test_achaContaErro():
+  criaConta('1')
+  assert achaConta('2') == -1
+  deletaConta('1')
+  return
+
+def test_setNomeOk():
+  criaConta('1')
+  assert setNome('1','joão') == "nome da conta 1 alterado para joão"
+  deletaConta('1')
+
+def test_setNomeErrado():
+  criaConta('1')
+  assert setNome('2','joão') == "Conta não encontrada"
+  deletaConta('1')
+
+def test_setCPFOK():
+  criaConta('1')
+  assert setCPF('1','09198745689') == "CPF da conta 1 alterado para 09198745689"
+  deletaConta('1')
+
+def test_setCPRFErrado():
+  criaConta('1')
+  assert setCPF('2', '09198745689') == "Conta não encontrada"
+  deletaConta('1')
+
+def test_deletaContaOK():
+  criaConta('1')
+  assert deletaConta('1') == "conta 1 deletada"
+
+def test_deletaContaErrada():
+  criaConta('1')
+  assert deletaConta('2') == "Conta não encontrada"
+  deletaConta('1')
+
+def testHub():
+  test_criaRepetido()
+  test_criaOK()
+  test_achaContaOK()
+  test_achaContaErro()
+  test_setNomeOk()
+  test_setNomeErrado()
+  test_setCPFOK()
+  test_setCPRFErrado()
+  test_deletaContaOK()
+  test_deletaContaErrada()
+
 id = []
 saldo = []
 nome = []
 cpf = []
 
 def main():
-  criaConta('4')
-  printaTodas()
-  print(achaConta('4'))
-  print(printaConta('4'))
+  testHub()
 
 if __name__ == "__main__":
     main()
