f039bbc7e9bfc3a2636235fe91fbca77995a4fea
lipeph99 <lipeph99@gmail.com>
2022-05-28 13:48:27 -0300

    cria acha e printa conta primeiro teste

--- main.py
+++ main.py
@@ -1,22 +1,50 @@
-def main():
+def criaConta(_id):
+  id.append(_id)
+  saldo.append(0)
+  nome.append('')
+  cpf.append('')
+  return
 
-  def criaConta(newId):
-    id.append(newId)
-    saldo.append(0)
-    return
+def printaTodas():
+  i=0
+  while (i<len(id)):
+    print(id[i],saldo[i])
+    i = i+1
+  return
+
+def achaConta(_id):
+  pos = -1
+  i=0
+  while (i<len(id)):
+    if id[i]==_id:
+      pos = i
+      break
+    i = i+1
+  return pos
+
+def printaConta(_id):
+  pos = achaConta(_id)
+  if(pos==-1):
+    return "Contra não encontrada"
+  else:
+    return "id = " + id[pos] + " saldo = " + str(saldo[pos]) + " nome = " + nome[pos]
   
-  def printaTodas():
-    i=0
-    while (i<len(id)):
-      print(id[i],saldo[i])
-      i = i+1
-    return
-  #Ler input e printar matriz
-  id = []
-  saldo = []
+def test_cria():
   criaConta('1')
-  criaConta('2')
+  assert printaConta('1') == "id = 1 saldo = 0 nome = "
+
+id = []
+saldo = []
+nome = []
+cpf = []
+
+def main():
+  criaConta('4')
   printaTodas()
+  print(achaConta('4'))
+  print(printaConta('4'))
+  test_cria()
 
 if __name__ == "__main__":
     main()
+
